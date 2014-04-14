b8526a4340a6a170100a5a1ee338eacadffd42a8
Nikhil Mathew <nikhilmat@gmail.com>
2014-04-14 11:09:34 -0400

    basic backbone

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+require.config({
+    paths: {
+        "jquery": ["http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min",
+                    "libs/jquery/jquery"],
+        "underscore": "libs/underscore/underscore",
+        "backbone": "libs/backbone/backbone"
+    },
+    shim: {
+        "backbone": {
+            deps: ["jquery", "underscore"],
+            exports: "Backbone"
+        }
+    },
+    waitSeconds: 10
+});
+
+require(['jquery', 'underscore', 'backbone', 'app'], function(jquery, _, Backbone, App){
+    new App;
+});
